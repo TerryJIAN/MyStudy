a0323e154be5c4b59e6e2858106f26e19279b398
TerryJIAN <kiss0817nice@gmail.com>
2020-08-06 14:13:32 +0800

    Update KafkaProducer.py

--- kafka/KafkaProducer.py
+++ kafka/KafkaProducer.py
@@ -2,6 +2,8 @@
 from time import sleep
 from json import dumps
 from kafka import KafkaProducer
+import time
+
 data =['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
 producer = KafkaProducer(bootstrap_servers=['192.168.105.49:9092'],
                          value_serializer=lambda x: 
